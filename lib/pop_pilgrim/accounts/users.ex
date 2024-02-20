2df2345db989a8425a482cf3f0ed7a4276f81e01
Gabrielparizet <parizetgabriel@gmail.com>
2024-02-20 18:03:50 +0100

    feat(accounts): Built schema for users table

---
+++ lib/pop_pilgrim/accounts/users.ex
@@ -0,0 +1,29 @@
+defmodule PopPilgrim.Accounts.Users do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  @primary_key false
+  schema "users" do
+    field(:id, :binary_id)
+    field(:username, :string)
+    field(:hashed_password, :string)
+    field(:email, :string)
+    field(:first_name, :string)
+    field(:last_name, :string)
+    field(:birthday, :utc_datetime)
+    field(:country_code, :string)
+
+    timestamps()
+  end
+
+  def changeset(user, attrs) do
+    user
+    |> cast(attrs, [:username, :email, :first_name, :last_name, :country_code])
+    |> validate_required([:username, :email, :first_name, :last_name, :country_code])
+    |> validate_length(:username, min: 1, max: 20)
+    |> validate_length(:email, max: 160)
+    |> validate_format(:email, ~r/^[^\s]+@[^\s]+$/, message: "must have the @ sign and no spaces")
+    |> unique_constraint([:username, :email])
+  end
+
+end
---
+++ priv/repo/migrations/20240220134232_add_users.exs
@@ -0,0 +1,20 @@
+defmodule PopPilgrim.Repo.Migrations.AddUsers do
+  use Ecto.Migration
+
+  def change do
+    create table(:users, primary_key: false, prefix: "public") do
+      add(:id, :uuid, primary_key: true)
+      add(:username, :string)
+      add(:hashed_password, :string)
+      add(:email, :string)
+      add(:first_name, :string)
+      add(:last_name, :string)
+      add(:birthday, :utc_datetime)
+      add(:country_code, :string)
+
+      timestamps(type: :utc_datetime)
+    end
+
+    create unique_index(:users, [:id, :username, :email])
+  end
+end
